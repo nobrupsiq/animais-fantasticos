f116e46f48f42be308a12f77ad6b4cf0e4bfeb2a
bruno <bpires.siqueira@gmail.com>
2023-04-04 18:54:37 -0300

    update

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,30 +1,32 @@
 export default function initAnimaNumeros() {
-    function animaNumeros() {
-    const numeros = document.querySelectorAll('[data-numero]');
+  function animaNumeros() {
+    const numeros = document.querySelectorAll("[data-numero]");
 
     numeros.forEach((numero) => {
-        const total = +numero.innerText;
-        const inscremento = Math.floor(total / 100);
-        let start = 0;
-        const timer = setInterval(() => {
-            start += inscremento;
-            numero.innerText = start;
-            if (start > total) {
-                numero.innerText = total
-                clearInterval(timer);
-            }
-        }, 25 * Math.random());
+      const total = +numero.innerText;
+      const inscremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start += inscremento;
+        numero.innerText = start;
+        if (start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random());
     });
-}
+  }
 
-function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains('ativo')) {
-        observer.disconnect();
-        animaNumeros();
-   }
-}
-const observerTarget = document.querySelector('.numeros');
-const observer = new MutationObserver(handleMutation);
+  let observer;
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains("ativo")) {
+      observer.disconnect();
+      animaNumeros();
+    }
+  }
 
-observer.observe(observerTarget, {attributes: true})
+  observer = new MutationObserver(handleMutation);
+  const observerTarget = document.querySelector(".numeros");
+
+  observer.observe(observerTarget, { attributes: true });
 }
--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,10 +1,11 @@
 export default function initFetchBitcoin() {
-    fetch('https://www.blockchain.com/ticker')
-        .then(response => response.json())
-        .then(json => {
-            const btcPreco = document.querySelector('.btc-preco')
-            btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
-        }).catch(erro => {
-            console.log(erro);
-        })
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(".btc-preco");
+      btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
 }
