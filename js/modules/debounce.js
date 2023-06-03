c0b35ae9ab6cd12e1ae78ed5c7478b9cd3d10c69
bruno <bpires.siqueira@gmail.com>
2023-06-02 21:22:08 -0300

    adicionado debounce na animação ao scroll

---
+++ js/modules/debounce.js
@@ -0,0 +1,12 @@
+// Função Helper
+
+export default function debounce(callback, delay) {
+    let timer;
+    return (...args) => {
+      if(timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(...args);
+        timer = null;
+      }, delay)
+    }
+  }
